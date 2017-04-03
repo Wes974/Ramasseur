7b3a07fa1f127d1dc0af1f3ac2066e34f1ddc0e4
Ouwéis <ouweis.m@icloud.com>
2017-04-03 23:43:02 +0400

    Add distance, motor command, and final Robot program + various updates of code

--- Ramasseur/Ramasseur.ino
+++ Ramasseur/Ramasseur.ino
@@ -1,19 +1,5 @@
 #include <Arduino.h>
 
-/*
- 5/22/2012
-  Timothy Holmberg
-  SparkFun Electronics
-
-  This code includes the ddition of fade in and out PWM. Also a stop feature. And the addition of individual functions for motor control
-
-  This was a revision of the example sketch that originated from Pete Dokter's code for Arduino that shows very basically how to control an Ardumoto
-  motor driver shield with a 5V Arduino controller board. http://www.sparkfun.com/datasheets/DevTools/Arduino/Ardumoto_test_3.pde
-
-  This also includes parts of the Fading Example,  Created 1 Nov 2008 By David A. Mellis, modified 30 Aug 2011 By Tom Igoe http://arduino.cc/en/Tutorial/Fading
-
-*/
-
 #include <SPI.h>
 #include <Pixy.h>
 
@@ -101,26 +87,27 @@ void loop() {
 
   // If there are detect blocks, print them!
   if (blocks) {
-    /*
+
     i++;
 
-    // do this (print) every 50 frames because printing every
-    // frame would bog down the Arduino
-    if (i%50==0)
-    {
-      sprintf(buf, "Detected %d:\n", blocks);
-      Serial.print(buf);
-      for (j=0; j<blocks; j++)
-      {
-        sprintf(buf, "  block %d: ", j);
-        Serial.print(buf);
-        pixy.blocks[j].print();
+    Serial.print(pixy.blocks[j].x);
+    Serial.print(" \n");
+
+    if (i%50==0) {
+
+      if (pixy.blocks[j].x < 140) {
+
+        Serial.print("Aller à gauche\n");
+        gauche();
+
+      } else if (pixy.blocks[j].x > 160) {
+
+        Serial.print("Aller à droite\n");
+        droite();
+
       }
     }
-    */
-    forward();
-    Serial.print("go to ball\n");
-    delay(500);
+    delay(100);
   }
 
 }
